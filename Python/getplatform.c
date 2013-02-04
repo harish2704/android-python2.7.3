6c10c71250f6554bc1d1cc14dac9c2163bd91947
Harish.K <harish@hari.in>
2013-02-04 20:19:26 +0530

    xcomile getpltform patch

--- Python/getplatform.c
+++ Python/getplatform.c
@@ -1,12 +1,25 @@
-
 #include "Python.h"
 
-#ifndef PLATFORM
-#define PLATFORM "unknown"
-#endif
+#include <sys/utsname.h>
+#include <string.h>
 
 const char *
 Py_GetPlatform(void)
 {
-	return PLATFORM;
+    struct utsname u;
+    int i;
+    if ( uname(&u) < 0 )
+	return "unknown";
+
+    char xx[37];
+    memset(xx, 0, 37);
+    strcat (xx, u.sysname);
+    strcat (xx, "-");
+    strcat (xx, u.machine);
+
+    for (i=0; xx[i]; i++)
+	xx[i]=tolower(xx[i]);
+
+    return xx;
 }
+
