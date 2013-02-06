7e9aa7ea38797786c6a110fccfc3c16f9d8c13a1
Harish.K <harish@hari.in>
2013-02-06 23:28:36 +0530

    updated readme.md

--- Readme.md
+++ Readme.md
@@ -2,9 +2,33 @@ Python2.7.3 Android
 ===================
 
 
-This is source for building python2.7.3 for android usig android-ndk.
+This is source for building python2.7.3 for android usig android-ndk.inspired by [python-for-android - Py4A] (http://code.google.com/p/python-for-android/)
+
+
 
 Binary files can be downloaded from [sourceforge download page](https://sourceforge.net/projects/androidpython27/)
 by using python we can run ipython on android terminal emulator
 
 compile.sh may give some clue about compiling the source.
+
+
+
+
+
+
+
+compiling hints
+===============
+
+Set the appropriate path for your android-ndk ,includes and libs in compile.sh
+put a runnable python2.7.3 binary file and rename it into hostpython.(It can be copied from /usr/bin or can be compiled from source)
+uncomment 'configure' and 'make' commands in compile.sh for corresponding actions.
+this will produce binaries with debugging information (big size tooo). so stripping down is necessary
+binaries with debugging symbols can be used for debugging with gdbsever.
+gdbserver arm-elf binary can be found in ndk folder
+file gdb.setup contains some gdb commands which i used for debuging python using gdb remote debugging
+For redline support, readline and ncurses libraries are necessary.
+no need to compile sqlite and openssl (I didn't do that)
+copy the required header files (for sqlite and openssl) from androidsource/external folder and copy shared libraries fom phones /system/lib folder
+
+
--- compile.sh
+++ compile.sh
@@ -1,14 +1,28 @@
 
+# Set the appropriate path for your android-ndk and for includes and libs
+# put a runnable python2.7.3 binary file and rename it into hostpython.(It can be copied from /usr/bin or can be compiled from source)
+# uncomment 'configure' and 'make' commands to corresponding actions.
+# this will produce binaries with debugging information (big size tooo). so stripping down is necessary
+# binaries with debugging symbols can be used for debugging with gdbsever.
+# gdbserver arm-elf binary can be found in ndk folder
+# file gdb.setup contains some gdb commands which i used for debuging python using gdb remote debugging
+# For redline support, readline and ncurses libraries are necessary.
+# no need to compile sqlite and openssl (I didn't do that)
+# copy the required header files (for sqlite and openssl) from androidsource/external folder and copy shared libraries fom phones /system/lib folder
+
+
 export PATH+=:/home/harish/android-ndk-r6b/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin
 export INCLUDES='/data/local/include:/home/harish/android-ndk-r6b/platforms/android-9/arch-arm/usr/include'
-export LIBS='/data/local/lib'
+export LIBS='/data/local/lib' 
 
 # ./configure --build=i686-pc-linux-gnu --host=arm-linux-androideabi --target=arm-linux-androideabi \
-CFLAGS="--sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -I/data/local/include -I/data/local/include/ncurses -I/data/local/include/readline -g3" \
-LDFLAGS="-g3 --sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -L/data/local/lib" \
-CXXFLAGS="-g3 --sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm" \
---prefix=/data/local/ \
-build_alias=i686-pc-linux-gnu host_alias=arm-linux-androideabi target_alias=arm-linux-androideabi \
-CC=arm-linux-androideabi-gcc --enable-unicode
+# CFLAGS="--sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -I/data/local/include -I/data/local/include/ncurses -I/data/local/include/readline -g3" \
+# LDFLAGS="-g3 --sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -L/data/local/lib" \
+# CXXFLAGS="-g3 --sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm" \
+# --prefix=/data/local/ \
+# build_alias=i686-pc-linux-gnu host_alias=arm-linux-androideabi target_alias=arm-linux-androideabi \
+# CC=arm-linux-androideabi-gcc --enable-unicode
 
-make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen BLDSHARED="arm-linux-androideabi-gcc -shared" CROSS_COMPILE=arm-linux-androideabi- CROSS_COMPILE_TARGET=yes HOSTARCH=arm-linux-androideabi BUILDARCH=i686-pc-linux-gnu  CFLAGS="--sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -I/data/local/include -I/data/local/include/ncurses -I/data/local/include/readline -g3 -I/home/harish/Downloads/Build/Python-3.2/forPy/openssl-android-master/include" LDFLAGS="--sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -L/data/local/lib -g3" CXXFLAGS="--sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -g3" ARCH=arm NDKPLATFORM="/home/harish/android-ndk-r6b/platforms/android-9"  clean
+# make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen BLDSHARED="arm-linux-androideabi-gcc -shared" CROSS_COMPILE=arm-linux-androideabi- CROSS_COMPILE_TARGET=yes HOSTARCH=arm-linux-androideabi BUILDARCH=i686-pc-linux-gnu  CFLAGS="--sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -I/data/local/include -I/data/local/include/ncurses -I/data/local/include/readline -g3 -I/home/harish/Downloads/Build/Python-3.2/forPy/openssl-android-master/include" LDFLAGS="--sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -L/data/local/lib -g3" CXXFLAGS="--sysroot=/home/harish/android-ndk-r6b/platforms/android-9/arch-arm -g3" ARCH=arm NDKPLATFORM="/home/harish/android-ndk-r6b/platforms/android-9"  clean
+
+
